fa1e73f08d59cce7a52f2e1f324150b995ef7a83
lyonsan <ryoga.yajima@gmail.com>
2021-04-17 18:14:09 +0900

    データ一覧表示(ただ表示するだけ)

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,21 @@
 class CategoriesController < ApplicationController
   def index
+    if params[:category_name]
+      @category = Category.find_by( name: params[:category_name] )
+      if  @category.present?
+        @ideas = Idea.where( category_id: @category.id )
+        render json: @ideas
+      else
+        render json: { status: 422 }
+      end
+    else
+      @ideas = Idea.all
+      render json: @ideas
+    end
   end
   def create
-    category = Category.find_by( name: params[:category_name] )
-    if category.present? && params[:body].present?
+    @category = Category.find_by( name: params[:category_name] )
+    if @category.present? && params[:body].present?
       @category = Category.find_by(name: params[:category_name])
       @idea = @category.ideas.build(idea_params)
       if @idea.save
