64fcdcc6efc97caeb503d9b23a18a66fedb0bb6f
lyonsan <ryoga.yajima@gmail.com>
2021-04-18 19:06:41 +0900

    case_sensitive

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
   has_many :ideas
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
